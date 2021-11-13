d39ce6c3f23fd0e5e482bd1135e7e1d14d8e4c30
64Bit-USER <92208342+64Bit-USER@users.noreply.github.com>
2021-11-13 20:24:47 +0530

    Updated

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = "mongodb+srv://dbMainUser:mern123@cluster0.fuhqz.mongodb.net/test";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => console.log('Connected To Mongo Successfully!'));
